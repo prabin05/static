8a14951420a37526a8069483603bce9be55d8af7
prabin05 <prabinregmi05@gmail.com>
2022-03-24 00:02:41 +0545

    js only no npm

--- site/index.js
+++ site/index.js
@@ -1,7 +1,7 @@
 var myStr = 'Hello World';
 
-console.log(myStr.length);
-// console.log(myStr.indexOf('ell'), myStr.indexOf('www'));
+// console.log(myStr.length);
+console.log(myStr.indexOf('ell'), myStr.indexOf('www'));
 // console.log(myStr[0], myStr[myStr.length - 1]);
 // console.log(myStr.concat('hello'), myStr);
 // console.log(myStr.split('ll'));

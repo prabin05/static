20b160cbdf16d2085370b867de7ac5ae7d3f703a
prabin05 <prabinregmi05@gmail.com>
2022-03-23 23:58:35 +0545

    JS and Npm install

---
+++ site/index.js
@@ -0,0 +1,9 @@
+var myStr = 'Hello World';
+
+console.log(myStr.length);
+// console.log(myStr.indexOf('ell'), myStr.indexOf('www'));
+// console.log(myStr[0], myStr[myStr.length - 1]);
+// console.log(myStr.concat('hello'), myStr);
+// console.log(myStr.split('ll'));
+// console.log(myStr.toUpperCase());
+// console.log(myStr.substr(1, 5));
